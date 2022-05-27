99139457d3348be85a3c2a267c9fa18a88fb5597
Prabesh <prabesh.bista369@gmail.com>
2022-05-27 18:55:47 +0545

    basic of typescript

---
+++ basic.ts
@@ -0,0 +1,8 @@
+//Primitive:number ,string ,boolean
+//More Complex types:arrays,objects
+//Functions types ,parameters
+
+//Primitives
+
+let age:number;
+age=20;
--- react.ts
+++ react.ts
@@ -2,6 +2,6 @@ function add(a:number,b:number){
     return a+b;
 }
 
-const result=add(2,3);
+var result=add(2,3);
 console.log(result);
 
